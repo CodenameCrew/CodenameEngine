ed7f2eb904c1fc32bbb25fdc52085c8a2bd3e3c4
SpareEnderboy <113299492+SpareEnderboy@users.noreply.github.com>
2025-04-04 19:19:40 -0500

    i can code too!
    press any key is a reality bitches

--- source/funkin/menus/BetaWarningState.hx
+++ source/funkin/menus/BetaWarningState.hx
@@ -19,7 +19,7 @@ class BetaWarningState extends MusicBeatState {
 
 		disclaimer = new FunkinText(16, titleAlphabet.y + titleAlphabet.height + 10, FlxG.width - 32, "", 32);
 		disclaimer.alignment = CENTER;
-		disclaimer.applyMarkup('McDonalds fries taste like booty crust. Do not eat them.',
+		disclaimer.applyMarkup('This engine is still in a *${Main.releaseCycle}* state. That means *majority of the features* are either *buggy* or *non finished*. If you find any bugs, please report them to the Codename Engine GitHub.\n\nPress any key to continue',
 			[
 				new FlxTextFormatMarkerPair(new FlxTextFormat(0xFFFF4444), "*")
 			]
@@ -36,12 +36,12 @@ class BetaWarningState extends MusicBeatState {
 	public override function update(elapsed:Float) {
 		super.update(elapsed);
 
-		if (controls.ACCEPT && transitioning) {
+		if (FlxG.keys.pressed.ANY && transitioning) {
 			FlxG.camera.stopFX(); FlxG.camera.visible = false;
 			goToTitle();
 		}
 
-		if (controls.ACCEPT && !transitioning) {
+		if (FlxG.keys.pressed.ANY && !transitioning) {
 			transitioning = true;
 			CoolUtil.playMenuSFX(CONFIRM);
 			FlxG.camera.flash(FlxColor.WHITE, 1, function() {
