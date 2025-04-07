cc663b37f62d8f3a996c8503485c506cd67450a3
Microkat <89645987+THEkatinamicrowave@users.noreply.github.com>
2025-04-07 12:29:48 -0400

    Open Pause Substate on Focus Lost

--- source/funkin/game/PlayState.hx
+++ source/funkin/game/PlayState.hx
@@ -1123,6 +1123,8 @@ class PlayState extends MusicBeatState
 		scripts.call("onFocusLost");
 		updateDiscordPresence();
 		super.onFocusLost();
+
+		openSubState(new PauseSubState());
 	}
 
 	@:dox(hide)
