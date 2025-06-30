5225611448c894e22b18e45e36ef36a1dadba8ad
Jade <45212377+crowplexus@users.noreply.github.com>
2025-06-30 08:07:29 -0400

    ok github.

--- assets/songs/monster/scripts/vignette.hx
+++ assets/songs/monster/scripts/vignette.hx
@@ -1,11 +1,8 @@
-function create()
-{
-	if (!Options.gameplayShaders)
-	{
+function create() {
+	if (!Options.gameplayShaders) {
 		disableScript();
 		return;
 	}
-
 	vignette = new CustomShader('coloredVignette');
 	vignette.color = [0, 0, 0];
 	vignette.amount = 1;
@@ -13,18 +10,15 @@ function create()
 	camGame.addShader(vignette);
 }
 
-function update(elapsed:Float)
-{
-	var targetStrength:Float = 1 - (health / 2) / 1.5;
-
+function update(elapsed:Float) {
 	var bg = stage.stageSprites.get('bg');
+	var targetStrength:Float = 1 - (health / 2) / 1.5;
 	if (bg.animation.name == 'lightning' && !bg.animation.finished)
 		vignette.strength = 0.075;
 	else
 		vignette.strength = lerp(vignette.strength, targetStrength, 0.1);
 }
 
-function onGameOver()
-{
+function onGameOver() {
 	FlxTween.tween(vignette, {"strength": 0.075}, 1, {ease: FlxEase.quadOut});
 }
