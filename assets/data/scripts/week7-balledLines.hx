e661ad928468dba0bf99b4f8e8a2b19a1189bdfe
⍚~Nex <87421482+NexIsDumb@users.noreply.github.com>
2025-07-04 21:51:10 +0200

    yes i will shut the fuck up now

--- assets/data/scripts/week7-balledLines.hx
+++ assets/data/scripts/week7-balledLines.hx
@@ -4,6 +4,6 @@ function create()
 	sound = Paths.sound('jeffGameover/jeffGameover-' + FlxG.random.int(1, 25, !Options.naughtyness ? [1, 3, 8, 13, 17, 21] : null));
 
 function beatHit(cur:Int) if (cur == 0) {
-	FlxG.sound.music.volume = 0.2;
 	FlxG.sound.play(sound, 1, false, null, true, () -> if (!isEnding) FlxG.sound.music.fadeIn(4, 0.2, 1));
+	FlxG.sound.music.volume = 0.2;
 }
