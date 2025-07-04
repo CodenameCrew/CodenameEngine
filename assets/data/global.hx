a814b55465512e83e3944dbd819d98d04e0d2770
ItsLJcool <54383469+ItsLJcool@users.noreply.github.com>
2025-07-04 14:21:23 -0700

    <y>forgor to edit assets global.hx<y>

--- assets/data/global.hx
+++ assets/data/global.hx
@@ -1,7 +1,3 @@
 //.... hey... sorry guys. I ated the code... -LJ
 
 // If you want to know how to use GlobalScript, well check the CodenameEngine wiki. https://codename-engine.com/wiki/modding/scripting/global-scripts
-
-function new() {
-	// trace("Hello from GlobalScript!");
-}
