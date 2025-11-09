69ea22edfe7b701a88707e0884df83ca479b360d
r6915ee <astergthefourth@gmail.com>
2025-11-08 18:10:57 -0800

    Allow Linux-compatible kernels to work with UNIX doc generation script
    Considering the variety of kernels available alongside ELF compatibility,
    the UNIX documentation generation script has been modified to only check
    for the Darwin kernel and assume every other kernel as Linux-compatible.

--- art/generate-docs-unix.sh
+++ art/generate-docs-unix.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env sh
 cd "$(dirname "$0")/.."
 echo Generating documentation...
-if [ $(uname) == "Linux" ]; then
-    echo "Platform is based on Linux"
-    haxelib run lime build linux --haxeflag="-xml docs/doc.xml" -D doc-gen -D DOCUMENTATION --no-output
-elif [ $(uname) == "Darwin" ]; then
+if [ $(uname) == "Darwin" ]; then
     echo "Platform is macOS"
     haxelib run lime build mac --haxeflag="-xml docs/doc.xml" -D doc-gen -D DOCUMENTATION --no-output
 else
-    echo "Platform $(uname) is incompatible with documentation generation, exiting..."
-    exit 126
+    echo "Platform is not macOS; assuming platform to be Linux compatible"
+    haxelib run lime build linux --haxeflag="-xml docs/doc.xml" -D doc-gen -D DOCUMENTATION --no-output
 fi
 
 echo "The XML file for the API documentation has been generated at docs/doc.xml."
