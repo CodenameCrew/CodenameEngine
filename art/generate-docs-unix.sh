3c0f1b24c5b2e4944f6cf83536e7487a55fabd99
r6915ee <astergthefourth@gmail.com>
2025-12-15 22:07:41 -0800

    Small consistency change in UNIX doc script when echoing pre-check message

--- art/generate-docs-unix.sh
+++ art/generate-docs-unix.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 cd "$(dirname "$0")/.."
-echo Generating documentation...
+echo "Generating documentation..."
 if [ $(uname) == "Darwin" ]; then
     echo "Platform is macOS"
     haxelib run lime build mac --haxeflag="-xml docs/doc.xml" -D doc-gen -D DOCUMENTATION --no-output
