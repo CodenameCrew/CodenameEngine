9b48bc182417baff8c47852989f96f3bb130e645
Rafi <mohamedrafikhan2009@gmail.com>
2026-03-28 11:16:16 +0000

    Fix the f#cking conflicts

--- source/funkin/game/cutscenes/VideoCutscene.hx
+++ source/funkin/game/cutscenes/VideoCutscene.hx
@@ -107,7 +107,7 @@ class VideoCutscene extends Cutscene {
 				File.saveBytes(localPath, Assets.getBytes(path));
 			}
 
-			if (video.load(localPath)) FlxTimer.wait(0.001, function() { mutex.acquire(); onReady(); mutex.release(); });
+			if (video.load(localPath)) new FlxTimer().start(0.001, function(_) { mutex.acquire(); onReady(); mutex.release(); });
 			else { mutex.acquire(); close(); mutex.release(); }
 		});
 
