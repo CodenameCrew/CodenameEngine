44f89184d0980f9105e625e22f9bf2daa4c8a7eb
Ralty <78720179+Raltyro@users.noreply.github.com>
2026-03-29 14:44:35 +0700

    Add Flag DEFAULT_SOUND_TIME_SCALED_PITCH

--- source/funkin/backend/system/Flags.hx
+++ source/funkin/backend/system/Flags.hx
@@ -107,6 +107,10 @@ class Flags {
 	public static var DEFAULT_STEPS_PER_BEAT:Int = 4;
 	public static var DEFAULT_LOOP_TIME:Float = 0.0;
 	public static var ICONS_AUTOPOSITION:Bool = true;
+
+	@:lazy public static var DEFAULT_SOUND_TIME_SCALED_PITCH:Null<Bool> = null;
+	@:lazy public static var USE_FLXTRAIL_FRAMES:Null<Bool> = null;
+
 	public static var SUPPORTED_CHART_RUNTIME_FORMATS:Array<String> = ["Legacy", "Psych Engine"];
 	public static var SUPPORTED_CHART_FORMATS:Array<String> = ["BaseGame"];
 
@@ -326,6 +330,11 @@ class Flags {
 				DEFAULT_GLSL_VERSION = openfl.utils.GLSLSourceAssembler.getDefaultVersion();
 			}
 		}
+		if (DEFAULT_SOUND_TIME_SCALED_PITCH == null) DEFAULT_SOUND_TIME_SCALED_PITCH = MOD_API_VERSION >= 2;
+		if (USE_FLXTRAIL_FRAMES == null) USE_FLXTRAIL_FRAMES = MOD_API_VERSION < 2;
+
+		flixel.sound.FlxSound.defaultTimeScaledPitch = cast DEFAULT_SOUND_TIME_SCALED_PITCH;
+		flixel.addons.effects.FlxTrail.defaultDelayBackwardCompatibility = cast USE_FLXTRAIL_FRAMES;
 	}
 
 	public static function loadFromDatas(datas:Array<String>):Map<String, String> {
