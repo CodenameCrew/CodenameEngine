1f4118b9a0a3ff410071e4ef81037385eb90ad08
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-05-20 19:12:14 -0700

    custom colors

--- source/mobile/backend/utils/MobileTrace.hx
+++ source/mobile/backend/utils/MobileTrace.hx
@@ -2,46 +2,37 @@ package mobile.backend.utils;
 
 import flixel.FlxG;
 import flixel.text.FlxText;
+import flixel.util.FlxColor;
 
 class MobileTrace
 {
 	public static var text:FlxText;
-	public static var logs:Array<String> = [];
-
-	public static var enabled:Bool = false;
 
 	public static function init()
 	{
-		if (text != null) return;
-
-		text = new FlxText(4, 4, FlxG.width - 8, "");
-
-		text.setFormat(null, 16, 0xFFFFFF00, LEFT);
-
-		text.alpha = 0.7;
-
+		text = new FlxText(10, 10, FlxG.width - 20, "");
+		text.setFormat(null, 16, FlxColor.YELLOW);
 		text.scrollFactor.set();
-
+		text.alpha = 0.7;
 		text.borderSize = 1;
-		text.borderColor = 0xFF000000;
-
-		text.cameras = [FlxG.cameras.list[0]];
+		text.cameras = [FlxG.cameras.list[FlxG.cameras.list.length - 1]];
 
 		FlxG.state.add(text);
 	}
 
-	public static function log(v:Dynamic)
+	public static function log(msg:Dynamic, ?color:FlxColor)
 	{
-		if (!enabled) return;
+		if (text == null) return;
 
-		if (text == null)
-			init();
+		if (color != null)
+			text.color = color;
 
-		logs.push(Std.string(v));
+		text.text += Std.string(msg) + "\n";
 
-		while (logs.length > 10)
-			logs.shift();
+		var lines = text.text.split("\n");
+		if (lines.length > 15)
+			lines.shift();
 
-		text.text = logs.join("\n");
+		text.text = lines.join("\n");
 	}
 }
