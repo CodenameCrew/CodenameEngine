686d6ee24e337f7e9a5a1240540d6ac11a9e7d30
MarioGuy <nenesantiago355@gmail.com>
2026-08-18 18:29:52 -0400

    we do not need this

--- source/funkin/game/HudCamera.hx
+++ source/funkin/game/HudCamera.hx
@@ -11,23 +11,6 @@ class HudCamera extends FlxCamera {
 	 * Keeps the sprites not flipped, but the positions are flipped.
 	 */
 	public var downscroll:Bool = false;
-	//public override function update(elapsed:Float) {
-	//	super.update(elapsed);
-	//	// flipY = downscroll;
-	//}
-
-
-	// public override function drawPixels(?frame:FlxFrame, ?pixels:BitmapData, matrix:FlxMatrix, ?transform:ColorTransform, ?blend:BlendMode, ?smoothing:Bool = false,
-	// 	?shader:FlxShader):Void
-	// {
-	// 	if (downscroll) {
-	// 		matrix.scale(1, -1);
-	// 		matrix.translate(0, height);
-	// 	}
-	// 	super.drawPixels(frame, pixels, matrix, transform, blend, smoothing, shader);
-	// }
-
-
 	public override function alterScreenPosition(spr:FlxObject, pos:FlxPoint) {
 		if (downscroll) {
 			pos.set(pos.x, height - pos.y - spr.height);
