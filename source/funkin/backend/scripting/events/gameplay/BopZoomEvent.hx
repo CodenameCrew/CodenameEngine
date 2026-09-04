bd185bda08d20bc5e0905e8eeaef6bb0d273ba02
sillyborja <sillyburgerprivalt@gmail.com>
2026-09-04 00:18:06 -0300

    zoom bop event (requires testing)

---
+++ source/funkin/backend/scripting/events/gameplay/BopZoomEvent.hx
@@ -0,0 +1,28 @@
+package funkin.backend.scripting.events.gameplay;
+
+final class BopZoomEvent extends CancellableEvent {
+	/**
+		Whether to use the zoomMultiplier property in the camera.
+	**/
+	public var useZoomMultiplier:Bool;
+
+	/**
+		If higher than this, the zoom will be skipped.
+	**/
+	public var maxZoomMultiplier:Float;
+
+	/**
+		The zoom's strength multiplier.
+	**/
+	public var zoomStrength:Float;
+
+	/**
+		The zoom's strength multiplier on the game camera.
+	**/
+	public var camGameZoomMultiplier:Float;
+
+	/**
+		The zoom's strength multiplier on the HUD camera.
+	**/
+	public var camHUDZoomMultiplier:Float;
+}
